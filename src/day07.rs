3d1f4c14cd32a6a4e28616e930199cd040ba2f7b
jcurtis <jamocurtis@gmail.com>
2022-12-07 21:34:46 -0400

    day 7 part 2
    Day7 - Part1/(default)  time:   [516.97 ns 525.79 ns 536.01 ns]
    Found 16 outliers among 100 measurements (16.00%)
      6 (6.00%) high mild
      10 (10.00%) high severe
    
    Day7 - Part2/(default)  time:   [670.89 ns 678.24 ns 686.49 ns]
    Found 6 outliers among 100 measurements (6.00%)
      4 (4.00%) high mild
      2 (2.00%) high severe

--- src/day07.rs
+++ src/day07.rs
@@ -3,28 +3,20 @@ use std::collections::HashMap;
 use itertools::Itertools;
 
 #[derive(Debug, Clone)]
-struct File {
-    size: u32,
-    name: String,
-}
-
-#[derive(Debug, Clone)]
 struct Dir {
     dirs: Vec<String>,
-    files: Vec<File>,
     size: u32,
 }
 
 type FileSystem = HashMap<Vec<String>, Dir>;
 
-#[aoc_generator(day7, part1)]
+#[aoc_generator(day7)]
 fn generator(input: &str) -> FileSystem {
     let mut file_system = HashMap::new();
     file_system.insert(
         vec!["/".to_string()],
         Dir {
             dirs: vec![],
-            files: vec![],
             size: 0,
         },
     );
@@ -49,7 +41,6 @@ fn generator(input: &str) -> FileSystem {
                             Some(d) => d.to_owned(),
                             None => Dir {
                                 dirs: vec![],
-                                files: vec![],
                                 size: 0,
                             },
                         };
@@ -68,13 +59,7 @@ fn generator(input: &str) -> FileSystem {
                 dir.dirs.push(line[1].to_string());
             }
             _ => {
-                let size = line[0].parse().unwrap();
-                let file = File {
-                    size,
-                    name: line[1].to_string(),
-                };
-                let dir = file_system.get_mut(&pwd).unwrap();
-                dir.files.push(file);
+                let size: u32 = line[0].parse().unwrap();
 
                 // bump all dir sizes
                 for i in 1..(pwd.len() + 1) {
@@ -103,8 +88,20 @@ fn part_1(input: &FileSystem) -> u32 {
 }
 
 #[aoc(day7, part2)]
-fn part_2(_input: &FileSystem) -> u32 {
-    todo!();
+fn part_2(input: &FileSystem) -> u32 {
+    let root = vec!["/".to_string()];
+    let to_free_size = input.get(&root).unwrap().size - 40000000;
+    input
+        .iter()
+        .filter_map(|(_, dir)| {
+            if dir.size >= to_free_size {
+                return Some(dir.size);
+            }
+            None
+        })
+        .sorted()
+        .next()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -126,6 +123,6 @@ mod tests {
         let input =
             fs::read_to_string("test_input/day07.txt").expect("Error reading test input file");
         let input = generator(&input);
-        assert_eq!(part_2(&input), 0);
+        assert_eq!(part_2(&input), 24933642);
     }
 }
