2918e754d614ed0a11f67cd841d074963b5bc336
jcurtis <jamocurtis@gmail.com>
2022-12-10 11:45:25 -0400

    day 10
    Day10 - Part1/(default) time:   [335.34 ns 336.05 ns 336.96 ns]
                            change: [-0.5944% -0.1191% +0.2797%] (p = 0.64 > 0.05)
                            No change in performance detected.
    Found 11 outliers among 100 measurements (11.00%)
      5 (5.00%) high mild
      6 (6.00%) high severe
    
    Day10 - Part2/(default) time:   [2.0115 us 2.0169 us 2.0229 us]
                            change: [-1.8253% -1.3197% -0.8861%] (p = 0.00 < 0.05)
                            Change within noise threshold.
    Found 7 outliers among 100 measurements (7.00%)
      5 (5.00%) high mild
      2 (2.00%) high severe

---
+++ src/day10.rs
@@ -0,0 +1,136 @@
+use itertools::Itertools;
+
+#[derive(Debug, PartialEq)]
+enum Cmd {
+    Noop,
+    PreAdd,
+    Addx,
+}
+
+type Ins = (Cmd, i32);
+
+#[aoc_generator(day10)]
+fn generator(input: &str) -> Vec<Ins> {
+    let mut x = 1;
+    input
+        .trim()
+        .lines()
+        .flat_map(|ins| {
+            let ins = ins.split_whitespace().collect_vec();
+            match ins[0] {
+                "noop" => {
+                    vec![(Cmd::Noop, x)]
+                }
+                "addx" => {
+                    let pre_x = x;
+                    let val: i32 = ins[1].parse().unwrap();
+                    x += val;
+                    vec![(Cmd::PreAdd, pre_x), (Cmd::Addx, pre_x)]
+                }
+                _ => unreachable!(),
+            }
+        })
+        .collect_vec()
+}
+
+#[aoc(day10, part1)]
+fn part_1(input: &[Ins]) -> i32 {
+    input
+        .iter()
+        .enumerate()
+        .filter_map(|(i, ins)| {
+            // println!("At {}: {:?}", i + 1, ins);
+            match i {
+                19 | 59 | 99 | 139 | 179 | 219 => {
+                    // println!("At {}: {:?}", i + 1, ins);
+                    Some((1 + i as i32) * ins.1)
+                }
+                _ => None,
+            }
+        })
+        .sum()
+}
+
+fn map_sprite(i: usize, ins: &Ins) -> char {
+    let cycle = i as i32;
+    let x = ins.1;
+    if x == cycle || x == cycle - 1 || x == cycle + 1 {
+        '#'
+    } else {
+        '.'
+    }
+}
+
+#[aoc(day10, part2)]
+fn part_2(input: &[Ins]) -> String {
+    (0..6)
+        .map(|i| {
+            input
+                .iter()
+                .skip(i * 40)
+                .take(40)
+                .enumerate()
+                .map(|(i, ins)| map_sprite(i, ins))
+                .collect::<String>()
+        })
+        .join("\n")
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::{Cmd, *};
+
+    #[test]
+    fn test_generator() {
+        let input = "noop
+        addx 3
+        addx -5";
+
+        assert_eq!(
+            generator(&input),
+            vec![
+                (Cmd::Noop, 1),
+                (Cmd::PreAdd, 1),
+                (Cmd::Addx, 1),
+                (Cmd::PreAdd, 4),
+                (Cmd::Addx, 4),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_part_1() {
+        let input =
+            fs::read_to_string("test_input/day10.txt").expect("Error reading test input file");
+        let input = generator(&input);
+        assert_eq!(part_1(&input), 13140);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input =
+            fs::read_to_string("test_input/day10.txt").expect("Error reading test input file");
+        let input = generator(&input);
+        // assert_eq!(
+        //     part_2(&input),
+        //     "##..##..##..##..##..##..##..##..##..##..###...###...###...###...###...###...###.####....####....####....####....####....#####.....#####.....#####.....#####.....######......######......######......##########.......#######.......#######.....");
+        assert_eq!(
+            part_2(&input),
+            "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."
+        );
+    }
+}
+
+// ####.####..##..####.###..#..#.###..####.
+// #....#....#..#.#....#..#.#..#.#..#.#....
+// ###..###..#....###..#..#.#..#.#..#.###..
+// #....#....#.##.#....###..#..#.###..#....
+// #....#....#..#.#....#.#..#..#.#.#..#....
+// ####.#.....###.####.#..#..##..#..#.####.
