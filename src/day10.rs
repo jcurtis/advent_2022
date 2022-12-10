5d8a6c7b18bd37ce005b82ec0efbf4a739b06882
jcurtis <jamocurtis@gmail.com>
2022-12-10 12:02:20 -0400

    simplify day 10

--- src/day10.rs
+++ src/day10.rs
@@ -1,16 +1,7 @@
 use itertools::Itertools;
 
-#[derive(Debug, PartialEq)]
-enum Cmd {
-    Noop,
-    PreAdd,
-    Addx,
-}
-
-type Ins = (Cmd, i32);
-
 #[aoc_generator(day10)]
-fn generator(input: &str) -> Vec<Ins> {
+fn generator(input: &str) -> Vec<i32> {
     let mut x = 1;
     input
         .trim()
@@ -19,31 +10,31 @@ fn generator(input: &str) -> Vec<Ins> {
             let ins = ins.split_whitespace().collect_vec();
             match ins[0] {
                 "noop" => {
-                    vec![(Cmd::Noop, x)]
+                    vec![x]
                 }
                 "addx" => {
                     let pre_x = x;
                     let val: i32 = ins[1].parse().unwrap();
                     x += val;
-                    vec![(Cmd::PreAdd, pre_x), (Cmd::Addx, pre_x)]
+                    vec![pre_x, pre_x]
                 }
                 _ => unreachable!(),
             }
         })
-        .collect_vec()
+        .collect()
 }
 
 #[aoc(day10, part1)]
-fn part_1(input: &[Ins]) -> i32 {
+fn part_1(input: &[i32]) -> i32 {
     input
         .iter()
         .enumerate()
-        .filter_map(|(i, ins)| {
+        .filter_map(|(i, x)| {
             // println!("At {}: {:?}", i + 1, ins);
             match i {
                 19 | 59 | 99 | 139 | 179 | 219 => {
                     // println!("At {}: {:?}", i + 1, ins);
-                    Some((1 + i as i32) * ins.1)
+                    Some((1 + i as i32) * x)
                 }
                 _ => None,
             }
@@ -51,9 +42,8 @@ fn part_1(input: &[Ins]) -> i32 {
         .sum()
 }
 
-fn map_sprite(i: usize, ins: &Ins) -> char {
+fn map_sprite(i: usize, x: i32) -> char {
     let cycle = i as i32;
-    let x = ins.1;
     if x == cycle || x == cycle - 1 || x == cycle + 1 {
         '#'
     } else {
@@ -62,7 +52,7 @@ fn map_sprite(i: usize, ins: &Ins) -> char {
 }
 
 #[aoc(day10, part2)]
-fn part_2(input: &[Ins]) -> String {
+fn part_2(input: &[i32]) -> String {
     (0..6)
         .map(|i| {
             input
@@ -70,7 +60,7 @@ fn part_2(input: &[Ins]) -> String {
                 .skip(i * 40)
                 .take(40)
                 .enumerate()
-                .map(|(i, ins)| map_sprite(i, ins))
+                .map(|(i, x)| map_sprite(i, *x))
                 .collect::<String>()
         })
         .join("\n")
@@ -80,7 +70,7 @@ fn part_2(input: &[Ins]) -> String {
 mod tests {
     use std::fs;
 
-    use super::{Cmd, *};
+    use super::*;
 
     #[test]
     fn test_generator() {
@@ -88,16 +78,7 @@ mod tests {
         addx 3
         addx -5";
 
-        assert_eq!(
-            generator(&input),
-            vec![
-                (Cmd::Noop, 1),
-                (Cmd::PreAdd, 1),
-                (Cmd::Addx, 1),
-                (Cmd::PreAdd, 4),
-                (Cmd::Addx, 4),
-            ]
-        );
+        assert_eq!(generator(&input), vec![1, 1, 1, 4, 4]);
     }
 
     #[test]
@@ -113,9 +94,6 @@ mod tests {
         let input =
             fs::read_to_string("test_input/day10.txt").expect("Error reading test input file");
         let input = generator(&input);
-        // assert_eq!(
-        //     part_2(&input),
-        //     "##..##..##..##..##..##..##..##..##..##..###...###...###...###...###...###...###.####....####....####....####....####....#####.....#####.....#####.....#####.....######......######......######......##########.......#######.......#######.....");
         assert_eq!(
             part_2(&input),
             "##..##..##..##..##..##..##..##..##..##..
