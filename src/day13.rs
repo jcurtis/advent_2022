d96ae973d6c24b894330d5e72d920432552b4259
jcurtis <jamocurtis@gmail.com>
2022-12-13 21:09:39 -0400

    day 13 part 2
    Day13 - Part1/(default) time:   [668.26 us 674.62 us 681.93 us]
    Found 12 outliers among 100 measurements (12.00%)
      2 (2.00%) low mild
      4 (4.00%) high mild
      6 (6.00%) high severe
    
    Day13 - Part2/(default) time:   [902.84 us 909.51 us 917.73 us]
    Found 12 outliers among 100 measurements (12.00%)
      3 (3.00%) low mild
      3 (3.00%) high mild
      6 (6.00%) high severe

--- src/day13.rs
+++ src/day13.rs
@@ -1,4 +1,7 @@
-use std::ops::ControlFlow::{self, Break, Continue};
+use std::{
+    cmp::Ordering::{Equal, Greater, Less},
+    ops::ControlFlow::{self, Break, Continue},
+};
 
 use itertools::{
     EitherOrBoth::{Both, Left, Right},
@@ -14,8 +17,8 @@ fn part_1(input: &str) -> usize {
         .enumerate()
         .filter_map(|(i, pairs)| {
             let pairs = pairs.split_whitespace().collect_vec();
-            let left = &serde_json::from_str(pairs[0]).unwrap();
-            let right = &serde_json::from_str(pairs[1]).unwrap();
+            let left: &Vec<Value> = &serde_json::from_str(pairs[0]).unwrap();
+            let right: &Vec<Value> = &serde_json::from_str(pairs[1]).unwrap();
             if solve_pair(left, right) {
                 Some(i + 1)
             } else {
@@ -26,18 +29,16 @@ fn part_1(input: &str) -> usize {
 }
 
 fn test(left: &Value, right: &Value) -> ControlFlow<bool> {
-    println!("Test {:?} vs {:?}", left, right);
+    // println!("Test {:?} vs {:?}", left, right);
     match (left, right) {
         (Value::Number(left), Value::Number(right)) => {
             let left = left.as_u64().unwrap();
             let right = right.as_u64().unwrap();
 
-            if left < right {
-                Break(true)
-            } else if left > right {
-                Break(false)
-            } else {
-                Continue(())
+            match left.cmp(&right) {
+                Less => Break(true),
+                Greater => Break(false),
+                Equal => Continue(()),
             }
         }
         (Value::Array(left), Value::Array(right)) => solve_pair_rec(left, right),
@@ -53,14 +54,14 @@ fn test(left: &Value, right: &Value) -> ControlFlow<bool> {
     }
 }
 
-fn solve_pair(input_left: &Vec<Value>, input_right: &Vec<Value>) -> bool {
+fn solve_pair(input_left: &[Value], input_right: &[Value]) -> bool {
     match solve_pair_rec(input_left, input_right) {
         Break(val) => val,
         Continue(_) => unreachable!(),
     }
 }
 
-fn solve_pair_rec(input_left: &Vec<Value>, input_right: &Vec<Value>) -> ControlFlow<bool> {
+fn solve_pair_rec(input_left: &[Value], input_right: &[Value]) -> ControlFlow<bool> {
     input_left
         .iter()
         .zip_longest(input_right.iter())
@@ -68,15 +69,40 @@ fn solve_pair_rec(input_left: &Vec<Value>, input_right: &Vec<Value>) -> ControlF
             Both(left, right) => test(left, right),
             // actually right
             Left(_) => Break(false),
-            // actuall left
+            // actually left
             Right(_) => Break(true),
         })
 }
 
-// #[aoc(day13, part2)]
-// fn part_2(_input: &str) -> u32 {
-//     todo!();
-// }
+const DIV_2: &str = "[[2]]";
+const DIV_6: &str = "[[6]]";
+
+#[aoc(day13, part2)]
+fn part_2(input: &str) -> u32 {
+    let res = [DIV_2, DIV_6, input]
+        .join("\n")
+        .lines()
+        .filter_map(|line| {
+            if line.is_empty() {
+                return None;
+            }
+            let value: Vec<Value> = serde_json::from_str(line).unwrap();
+            Some(value)
+        })
+        .sorted_by(|left, right| match solve_pair(left, right) {
+            true => Less,
+            false => Greater,
+        })
+        .collect_vec();
+
+    let div_2: Vec<Value> = serde_json::from_str(DIV_2).unwrap();
+    let div_6: Vec<Value> = serde_json::from_str(DIV_6).unwrap();
+
+    let div_2_pos = res.iter().position(|item| item == &div_2).unwrap() as u32;
+    let div_6_pos = res.iter().position(|item| item == &div_6).unwrap() as u32;
+
+    (div_2_pos + 1) * (div_6_pos + 1)
+}
 
 #[cfg(test)]
 mod tests {
@@ -91,77 +117,51 @@ mod tests {
         assert_eq!(part_1(&input), 13);
     }
 
+    fn from_str(input: &str) -> Vec<Value> {
+        serde_json::from_str(input).unwrap()
+    }
+
     #[test]
     fn test_solve_pair() {
         assert_eq!(
-            solve_pair(
-                &serde_json::from_str("[1,1,3,1,1]").unwrap(),
-                &serde_json::from_str("[1,1,5,1,1]").unwrap()
-            ),
+            solve_pair(&from_str("[1,1,3,1,1]"), &from_str("[1,1,5,1,1]")),
             true
         );
 
         assert_eq!(
-            solve_pair(
-                &serde_json::from_str("[[1],[2,3,4]]").unwrap(),
-                &serde_json::from_str("[[1],4]").unwrap()
-            ),
+            solve_pair(&from_str("[[1],[2,3,4]]"), &from_str("[[1],4]")),
+            true
+        );
+
+        assert_eq!(solve_pair(&from_str("[9]"), &from_str("[[8,7,6]]")), false);
+
+        assert_eq!(
+            solve_pair(&from_str("[[4,4],4,4]"), &from_str("[[4,4],4,4,4]")),
             true
         );
 
         assert_eq!(
-            solve_pair(
-                &serde_json::from_str("[9]").unwrap(),
-                &serde_json::from_str("[[8,7,6]]").unwrap()
-            ),
+            solve_pair(&from_str("[7,7,7,7]"), &from_str("[7,7,7]")),
             false
         );
 
-        assert_eq!(
-            solve_pair(
-                &serde_json::from_str("[[4,4],4,4]").unwrap(),
-                &serde_json::from_str("[[4,4],4,4,4]").unwrap()
-            ),
-            true
-        );
+        assert_eq!(solve_pair(&from_str("[]"), &from_str("[3]")), true);
+
+        assert_eq!(solve_pair(&from_str("[[[]]]"), &from_str("[[]]")), false);
 
         assert_eq!(
             solve_pair(
-                &serde_json::from_str("[7,7,7,7]").unwrap(),
-                &serde_json::from_str("[7,7,7]").unwrap()
-            ),
-            false
-        );
-
-        assert_eq!(
-            solve_pair(
-                &serde_json::from_str("[]").unwrap(),
-                &serde_json::from_str("[3]").unwrap()
-            ),
-            true
-        );
-
-        assert_eq!(
-            solve_pair(
-                &serde_json::from_str("[[[]]]").unwrap(),
-                &serde_json::from_str("[[]]").unwrap()
-            ),
-            false
-        );
-
-        assert_eq!(
-            solve_pair(
-                &serde_json::from_str("[1,[2,[3,[4,[5,6,7]]]],8,9]").unwrap(),
-                &serde_json::from_str("[1,[2,[3,[4,[5,6,0]]]],8,9]").unwrap()
+                &from_str("[1,[2,[3,[4,[5,6,7]]]],8,9]"),
+                &from_str("[1,[2,[3,[4,[5,6,0]]]],8,9]")
             ),
             false
         );
     }
 
-    // #[test]
-    // fn test_part_2() {
-    //     let input =
-    //         fs::read_to_string("test_input/day13.txt").expect("Error reading test input file");
-    //     assert_eq!(part_2(&input), 0);
-    // }
+    #[test]
+    fn test_part_2() {
+        let input =
+            fs::read_to_string("test_input/day13.txt").expect("Error reading test input file");
+        assert_eq!(part_2(&input), 140);
+    }
 }
